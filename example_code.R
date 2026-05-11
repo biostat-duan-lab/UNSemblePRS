48d8aafde8f517be969e98bc2f1a054d5f681812
Yu-Jyun (Gene) Huang <genehuang30648@gmail.com>
2026-05-11 11:06:30 -0400

    step by step example code
    step by step example code

--- example_code.R
+++ example_code.R
@@ -1,12 +1,15 @@
-#####################################################
+############################################################
 ## Example code
 ## Implementation of UNSemblePRS
-#####################################################
+############################################################
+## This script simulates genotype data, 
+## creates multiple noisy pre-trained PRS models, aggregates them using UNSemblePRS,
+## and compares correlations with the true simulated PRS.
+############################################################
 
 ## Required R packages
 library(kernlab)
 library(sparsepca)
-
 
 ## Main function: UNSemblePRS()
 ## Input:
@@ -47,15 +50,13 @@ UNSemblePRS <- function(X, standardize = TRUE, prop = 1){
     ))
 }
 
+############################################################
+## 1. Simulate genotype and true genetic effect data
+############################################################
 
+set.seed(12345)
 
-#####################################################
-## Simulation example
-#####################################################
-
-set.seed(123)
-
-N <- 1000
+N <- 10000
 p <- 100
 
 ## Generate genotype matrix
@@ -67,12 +68,16 @@ True_beta <- rnorm(p, 0, 1)
 ## True PRS
 True_PRS <- G_mat %*% True_beta
 
+############################################################
+## 2. Create multiple pre-trained PRS models
 ## Simulate 5 PRS pre-trained models with different noise levels
-PGS1_beta <- True_beta + rnorm(p, 0, 0.001)
-PGS2_beta <- True_beta + rnorm(p, 0, 0.1)
-PGS3_beta <- True_beta + rnorm(p, 0, 1)
-PGS4_beta <- True_beta + rnorm(p, 0, 3)
-PGS5_beta <- True_beta + rnorm(p, 0, 5)
+############################################################
+
+PGS1_beta <- True_beta + rnorm(p, 0, 0.1)
+PGS2_beta <- True_beta + rnorm(p, 0, 1)
+PGS3_beta <- True_beta + rnorm(p, 0, 5)
+PGS4_beta <- True_beta + rnorm(p, 0, 7)
+PGS5_beta <- True_beta + rnorm(p, 0, 9)
 
 ## Compute predicted PRSs
 PGS1 <- G_mat %*% PGS1_beta
@@ -81,29 +86,43 @@ PGS3 <- G_mat %*% PGS3_beta
 PGS4 <- G_mat %*% PGS4_beta
 PGS5 <- G_mat %*% PGS5_beta
 
-## Apply UNSemblePRS
-input <- cbind(PGS1, PGS2, PGS3, PGS4, PGS5)
 
-output <- UNSemblePRS(input)
+############################################################
+## 3. Apply UNSemblePRS
+############################################################
 
+prs_matrix <- cbind(PGS1, PGS2, PGS3, PGS4, PGS5)
+colnames(prs_matrix) <- paste0("PGS", seq_len(5))
+
+output <- UNSemblePRS(prs_matrix)
+
+## Aggregation weights for the input PRS models
 output$weight
 
+############################################################
+## 4. Compare performance with individual PRS models
+############################################################
 
 ## Evaluate performance
-eval_mat <- cbind(
+eval_mat <- data.frame(
   True_PRS = True_PRS,
   UNSemble = output$UNSemble,
-  PGS1 = PGS1,
-  PGS2 = PGS2,
-  PGS3 = PGS3,
-  PGS4 = PGS4,
-  PGS5 = PGS5
+  prs_matrix
 )
 
-round(cor(eval_mat), 3)
+correlation_matrix <- round(cor(eval_mat), 3)
+print(correlation_matrix)
 
+## Correlation of each score with the true PRS
+## Higher values indicate better recovery of the simulated true genetic score
+performance_summary <- data.frame(
+  score = colnames(eval_mat)[-1],
+  correlation_with_true_prs = correlation_matrix[-1, "True_PRS"],
+  row.names = NULL)
 
+performance_summary <- performance_summary[order(performance_summary$correlation_with_true_prs, decreasing = TRUE), ]
 
+print(performance_summary)
 
 
 
